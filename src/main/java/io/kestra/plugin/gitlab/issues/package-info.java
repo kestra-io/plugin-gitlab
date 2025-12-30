080a8e268b7f6c411ce0eb983d42d2980ffa0f92
ben8t <pimpaudben@gmail.com>
2025-12-30 15:37:54 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/gitlab/issues/package-info.java
+++ src/main/java/io/kestra/plugin/gitlab/issues/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     title = "Gitlab Issues",
     description = "This sub-group of plugins contains tasks for using GitLab.\nSet the GitLab host, project ID, and access token to file issues with title/description, add comments or updates, and list or retrieve issue details for triage workflows.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE,
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.gitlab.issues;
 
