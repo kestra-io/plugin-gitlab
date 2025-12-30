39d88b94900b1de5fe375058168ce476bd316c01
Malay Dewangan <malaydewangan310@gmail.com>
2025-12-30 13:32:55 +0530

    docs: add missing package-info

---
+++ src/main/java/io/kestra/plugin/gitlab/mergerequests/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Gitlab Merge Requests",
+    description = "Tasks that create, update, and fetch GitLab Merge Requests.\nSet the GitLab host, project ID, and access token to handle Merge Requests.",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.gitlab.mergerequests;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
