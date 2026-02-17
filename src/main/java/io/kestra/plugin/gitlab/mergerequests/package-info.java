6f6fd6b964ee087d4f92c571ce433607dd79fcef
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-17 09:50:17 +0100

    fix: gitlab.mergerequests categories

--- src/main/java/io/kestra/plugin/gitlab/mergerequests/package-info.java
+++ src/main/java/io/kestra/plugin/gitlab/mergerequests/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "Merge Requests",
     description = "Tasks that create, update, and fetch GitLab Merge Requests.\nSet the GitLab host name, project ID, and access token to handle Merge Requests.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE,
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.gitlab.mergerequests;
 
